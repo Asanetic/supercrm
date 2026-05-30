01e9c1eaccca66c52ff6638b53bf774fc23ac585
Asanetic <39214296+Asanetic@users.noreply.github.com>
2026-05-30 13:02:58 +0300

    Initial commit for supercrmv3  agent

--- app/api/apiUtils/s_env/bloomcrm_env.js
+++ app/api/apiUtils/s_env/bloomcrm_env.js
@@ -10,7 +10,7 @@ const mosyDbConfig = {
   production: {
     DB_HOST: '127.0.0.1',
     DB_USER: 'nextadmin',
-    DB_PASS: 'nextadmin001',
+    DB_PASS: 'UltraSecurePass123!',
     DB_NAME: 'bloomcrm',
     dateStrings: true
 
