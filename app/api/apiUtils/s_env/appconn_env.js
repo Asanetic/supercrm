f22edca9c5a9b2d71f5bdaa8414581f247a96bb5
Asanetic <39214296+Asanetic@users.noreply.github.com>
2026-08-04 05:12:03 +0300

    Initial commit for supercrmv8  agent

--- app/api/apiUtils/s_env/appconn_env.js
+++ app/api/apiUtils/s_env/appconn_env.js
@@ -9,7 +9,7 @@ const mosyDbConfig = {
   },
   production: {
     DB_HOST: '127.0.0.1',
-    DB_USER: 'root',
+    DB_USER: 'nextadmin',
     DB_PASS: 'UltraSecurePass123!',
     DB_NAME: 'supercrm',
     dateStrings: true
