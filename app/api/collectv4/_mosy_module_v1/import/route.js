c22461440be1733474c5a02f626b54bf71d4a289
Asanetic <39214296+Asanetic@users.noreply.github.com>
2026-08-15 06:44:20 +0300

    Initial commit for supercrmv10  agent

--- app/api/collectv4/_mosy_module_v1/import/route.js
+++ app/api/collectv4/_mosy_module_v1/import/route.js
@@ -2,7 +2,7 @@
 import { magicRandomStr, mosySqlInsert, processImport } from "../../../apiUtils/dataControl/dataUtils";
 import { processAuthToken } from "../../../auth/authManager";
 import { mutateInputArray } from "../../beMonitor";
-import { Smarttemplatev1Schema as schema } from '../schema';
+import { Smarttemplatev1Schema as schema } from './Smarttemplatev1Schema';
 
 //role access control 
 
